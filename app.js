876e243a7ed5725515391131da2019fdab56e703
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-11-09 03:51:41 +0700

    Fix xong toàn bộ rồi á

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const util = require( 'util');
 const { tong} = require('../github/ukm');
 var a =10; var b = 30;
 console.log(util.format('tổng %d+%d=%d',  a,b, tong(a, b)))
+console.log(util.format('tru %d+%d=%d',  a,b, tong(a, b))) 
