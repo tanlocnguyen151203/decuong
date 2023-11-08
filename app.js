46c4bfd3e0a5b11f01dacf73d30df896aa1cc03a
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-11-09 03:54:17 +0700

    Change a = 40

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const util = require( 'util');
 const { tong} = require('../github/ukm');
-var a =10; var b = 30;
+var a =40; var b = 30;
 console.log(util.format('tổng %d+%d=%d',  a,b, tong(a, b)))
-console.log(util.format('tru %d+%d=%d',  a,b, tong(a, b))) 
