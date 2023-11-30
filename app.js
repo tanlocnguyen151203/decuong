a6c5c127edbd40196db9e8cadb3097714086855b
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-11-30 14:09:26 +0700

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const util = require( 'util');
 const { tong, tru } = require('./ukm');
 
 var a ='40'; var b = 30;
-var a ='40'; var b = 30;
+var a ='40'; var b = 20;
 console.log(util.format('tổng %d+%d=%d',  a,b, tong(a, b)))
 console.log(util.format('trừ %d-%d=%d',  a,b, tru(a, b)))
