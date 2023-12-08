a98cb7ec5a375417eaa3dcbf08c7ab60014c5e2c
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-12-08 12:49:58 +0700

    .

--- bai.js
+++
@@ -1,7 +0,0 @@
-const util = require( 'util');
-const { tong, tru } = require('./ukm');
-
-var a ='40'; var b = 30;
-var a ='40'; var b = 50;
-console.log(util.format('tổng %d+%d=%d',  a,b, tong(a, b)))
-console.log(util.format('trừ %d-%d=%d',  a,b, tru(a, b)))
