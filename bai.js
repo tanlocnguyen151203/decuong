ce8ccb8a9fd2533ad859855974db5c4e200970fe
tanlocnguyen151203 <tanlocnguyen12003@gmail.com>
2023-12-09 15:06:25 +0700

    Update bai.js

--- bai.js
+++ bai.js
@@ -1,7 +1,10 @@
 const util = require( 'util');
 const { tong, tru } = require('./ukm');
 
-var a ='40'; var b = 10;
-var a ='40'; var b = 50;
+var a ='40'; var b = 20;
+
 console.log(util.format('tổng %d+%d=%d',  a,b, tong(a, b)))
+
+var a ='40'; var b = 50;
+
 console.log(util.format('trừ %d-%d=%d',  a,b, tru(a, b)))
